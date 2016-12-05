90628084f21443e0bad339d09830474634103180
zhang <zhangwei_w8284@proware.net>
2016-12-05 19:38:51 +0800

    restore app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  <LineG2></LineG2>
+  </div>
+</template>
+
+<script>
+import LineG2 from './components/LineG2'
+
+export default {
+  name: 'app',
+  components: {
+    LineG2
+  },
+  data () {
+    return {
+      data : [
+        {action: '访问', visitor: 500},
+        {action: '浏览', visitor: 400},
+        {action: '交互', visitor: 300},
+        {action: '下单', visitor: 200},
+        {action: '付款', visitor: 100}
+        ]
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
